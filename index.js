462397106baf0cefc146b4f63ccbac235ff8e795
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-11 15:42:26 -0400

    working on html and css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const bootstrap = require('bootstrap');
 
 const employees = [];
 
---
+++ src/renderHtml.js
@@ -0,0 +1,32 @@
+const bootstrap = require('bootstrap');
+
+`
+<!DOCTYPE html>
+<html lang="en>
+<head>
+<meta charset="utf-8>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Team Profile</title>
+<meta name="team profile" content="A profile view of the members of your team">
+<meta name="Lyndsie Lane" content="Profiles">
+<link rel="stylesheet" href="../dist/style.css">
+</head>
+<body>
+<nav class="navbar navbar-light bg-light" id="title">
+  <div class="container-fluid">
+    <span class="navbar-text">
+      Team Profiles
+    </span>
+  </div>
+</nav>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Employee Title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Employee Name</h6>
+    <p class="card-text">Employee Details</p>
+  </div>
+</div>
+
+<script src="./index.js"
+</body>
+</html>`

8b17e65a535fb98d9d90142a2dc3bf2fc97246fb
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-11 13:18:28 -0400

    one class and test in place

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,21 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should return an employee object including name, id, and email", () => {
+            const obj = new Employee();
+
+            expect("name" in obj).toEqual(true);
+            expect("id" in obj).toEqual(true);
+            expect("email" in obj).toEqual(true);
+        });
+
+        it("should return the given name, id, and email from the constructor", () => {
+            const obj = new Employee('45', 'Lyndsie', 'lyndsielane@yahoo.com');
+
+            expect(obj.getName()).toEqual('Lyndsie');
+            expect(obj.getId()).toEqual('45');
+            expect(obj.getEmail()).toEqual('lyndsielane@yahoo.com');
+        });
+    });
+});
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./Employee');
+const Employee = require('./lib/Employee');
 
 function init() {
     const employee = new Employee(1234, 'Alex', 'alex@thelanelife.com');

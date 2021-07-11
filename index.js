316c6b2054086cf728d13fb0cdc3e227e9be7bb8
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-11 16:50:00 -0400

    html rendering

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+//calling in required files and apps
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const bootstrap = require('bootstrap');
-
+const renderHtml = require('./src/renderHtml')
 const employees = [];
-
+//setting up the questions for the different roles
 const managerQuestions = [{
     type: 'input',
     message: "What is your manager's name?",
@@ -69,20 +69,20 @@ const internQuestions = [{
     message: "What is your intern's school name?",
     name: 'school'
 }];
-
+//setting up the question to continue building the team
 const addNewEmployeeQuestion = [{
     type: 'list',
     message: "Which type of team member would you like to add?",
     choices: ["Engineer", "Intern", "I don't want to add any more team members"],
     name: 'employeeType'
 }];
-
+//asking the questions and building the data received
 async function init() {
     const newManager = await inquirer.prompt(managerQuestions);
     employees.push(new Manager(newManager.id, newManager.name, newManager.email, newManager.officeNumber));
 
     let addNewEmployee = {};
-
+    //looping the questions back through based on the response given to the add more members questions
     while (addNewEmployee.employeeType !== 'I don\'t want to add any more team members') {
         addNewEmployee = await inquirer.prompt(addNewEmployeeQuestion);
 
@@ -97,8 +97,10 @@ async function init() {
                 break;
         }
     }
-
-    console.log(employees);
+    fs.writeFile('index.html', renderHtml(employees), (err) => {
+        if (err) console.log(error);
+        else console.log("File successfully created.");
+    });
 }
-
+//call function to initiate everything
 init();

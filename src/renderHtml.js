df324a126607cc44f3f5e3d73ddb8feb0b402382
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-12 16:40:02 -0400

    more formatting added

--- src/renderHtml.js
+++ src/renderHtml.js
@@ -28,6 +28,9 @@ return `
     <title>Team Profile</title>
     <meta name="team profile" content="A profile view of the members of your team">
     <meta name="Lyndsie Lane" content="Profiles">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Otomanopee+One&family=Raleway:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">

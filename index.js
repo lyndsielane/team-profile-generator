00308dd8b7aafad75e4a0e361eb6b8599449465f
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-13 16:08:00 -0400

    chalk added for inquirer styling

--- index.js
+++ index.js
@@ -4,76 +4,78 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const renderHtml = require('./src/renderHtml')
+const renderHtml = require('./src/renderHtml');
+const chalk = require('chalk');
 const employees = [];
+
 //setting up the questions for the different roles
 const managerQuestions = [{
     type: 'input',
-    message: "What is your manager's name?",
+    message: chalk.magenta("What is your manager's name?"),
     name: 'name'
 },
 {
     type: 'input',
-    message: "What is your manager's ID?",
+    message: chalk.magenta("What is your manager's ID?"),
     name: 'id'
 },
 {
     type: 'input',
-    message: "What is your manager's email?",
+    message: chalk.magenta("What is your manager's email?"),
     name: 'email'
 },
 {
     type: 'input',
-    message: "What is your manager's office number?",
+    message: chalk.magenta("What is your manager's office number?"),
     name: 'officeNumber'
 }];
 
 const engineerQuestions = [{
     type: 'input',
-    message: "What is your engineer's name?",
+    message: chalk.blueBright("What is your engineer's name?"),
     name: 'name'
 },
 {
     type: 'input',
-    message: "What is your engineer's ID?",
+    message: chalk.blueBright("What is your engineer's ID?"),
     name: 'id'
 },
 {
     type: 'input',
-    message: "What is your engineer's email?",
+    message: chalk.blueBright("What is your engineer's email?"),
     name: 'email'
 },
 {
     type: 'input',
-    message: "What is your engineer's Github username?",
+    message: chalk.blueBright("What is your engineer's Github username?"),
     name: 'github'
 }];
 
 const internQuestions = [{
     type: 'input',
-    message: "What is your intern's name?",
+    message: chalk.green("What is your intern's name?"),
     name: 'name'
 },
 {
     type: 'input',
-    message: "What is your intern's ID?",
+    message: chalk.green("What is your intern's ID?"),
     name: 'id'
 },
 {
     type: 'input',
-    message: "What is your intern's email?",
+    message: chalk.green("What is your intern's email?"),
     name: 'email'
 },
 {
     type: 'input',
-    message: "What is your intern's school name?",
+    message: chalk.green("What is your intern's school name?"),
     name: 'school'
 }];
 //setting up the question to continue building the team
 const addNewEmployeeQuestion = [{
     type: 'list',
-    message: "Which type of team member would you like to add?",
-    choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+    message: chalk.yellow("Which type of team member would you like to add?"),
+    choices: [chalk.blueBright("Engineer"), chalk.green("Intern"), chalk.red("I don't want to add any more team members")],
     name: 'employeeType'
 }];
 //asking the questions and building the data received
@@ -99,7 +101,7 @@ async function init() {
     }
     fs.writeFile('dist/team.html', renderHtml(employees), (err) => {
         if (err) console.log(error);
-        else console.log("File successfully created.");
+        else console.log(chalk.cyanBright("File successfully created."));
     });
 }
 //call function to initiate everything
